f7c260c8907ce89ac440faed262b00e94f195c92
Christoph Burki <chrisburki@gmx.ch>
2019-01-05 15:08:33 +0100

    current version

---
+++ src/k8/ingress/ingress-controller3/install.sh
@@ -0,0 +1,5 @@
+#see https://kubernetes.github.io/ingress-nginx/deploy/
+#kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+#delete ingress controller: kubectl delete namespace nginx-ingress
